89f6f38497bcc0ba1d9d057a994734c1f49532ea
Terence <terence@fixico.nl>
2017-12-15 12:30:08 +0100

    Day 15

---
+++ day-15-dueling-generators/one.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+type Generator struct {
+	Value  int
+	Factor int
+}
+
+func (g *Generator) Generate() string {
+	g.Value = (g.Value * g.Factor) % 2147483647
+	bin := fmt.Sprintf("%b", g.Value)
+	bin = fmt.Sprintf("%030s", bin)
+	return bin[len(bin)-16:]
+}
+
+func NewGenerator(v int, f int) *Generator {
+	return &Generator{
+		Value:  v,
+		Factor: f,
+	}
+}
+
+func main() {
+	a := NewGenerator(703, 16807)
+	b := NewGenerator(516, 48271)
+
+	count := 0
+	pairs := 40 * 1000 * 1000
+	for i := 0; i < pairs; i++ {
+		if i%1000000 == 0 {
+			fmt.Printf("i = %+v\n", i)
+		}
+		if a.Generate() == b.Generate() {
+			count++
+		}
+	}
+	fmt.Printf("count = %+v\n", count)
+}
